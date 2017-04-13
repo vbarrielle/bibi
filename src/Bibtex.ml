f361ca3aa97e9f512f3e36d3091361b658bc2408
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-04-13 15:27:00 +0200

    better error reporting

--- src/Bibtex.ml
+++ src/Bibtex.ml
@@ -30,6 +30,40 @@ let of_list = ID_map.of_list
 let mem = ID_map.mem
 let get = ID_map.get
 
+module Loc = struct
+  type t = {
+    file : string;
+    start_line : int;
+    start_column : int;
+    stop_line : int;
+    stop_column : int;
+  }
+
+  let mk file start_line start_column stop_line stop_column =
+    { file; start_line; start_column; stop_line; stop_column; }
+
+  let of_lexbuf lexbuf =
+    let start = Lexing.lexeme_start_p lexbuf in
+    let end_ = Lexing.lexeme_end_p lexbuf in
+    let s_l = start.Lexing.pos_lnum in
+    let s_c = start.Lexing.pos_cnum - start.Lexing.pos_bol in
+    let e_l = end_.Lexing.pos_lnum in
+    let e_c = end_.Lexing.pos_cnum - end_.Lexing.pos_bol in
+    let file = start.Lexing.pos_fname in
+    mk file s_l s_c e_l e_c
+
+  let pp out pos =
+    if pos.start_line = pos.stop_line
+    then
+      Format.fprintf out "@[file '%s': line %d, col %d to %d@]"
+        pos.file pos.start_line pos.start_column pos.stop_column
+    else
+      Format.fprintf out "@[file '%s': line %d, col %d to line %d, col %d@]"
+        pos.file
+        pos.start_line pos.start_column
+        pos.stop_line pos.stop_column
+end
+
 let parse lexbuf =
   try
     Bibtex_parse.entries Bibtex_lex.main lexbuf
@@ -41,8 +75,11 @@ let parse lexbuf =
          id, {e_kind; e_fields})
     |> of_list
     |> E.return
-
-  with e -> E.of_exn_trace e
+  with
+    | Bibtex_parse.Error ->
+      let pos = Loc.of_lexbuf lexbuf in
+      CCFormat.ksprintf ~f:E.fail "parse error %a" Loc.pp pos
+    | e -> E.of_exn_trace e
 
 let parse_file file =
   try
